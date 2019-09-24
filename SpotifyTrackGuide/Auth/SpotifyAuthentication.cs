533016181de4ee77b041351a36f8a244e8505ab8
Carlos Navarro <carlos.navarro@faculty.ancoraed.com>
2019-09-24 09:36:36 -0500

    changes

---
+++ SpotifyTrackGuide/Auth/SpotifyAuthentication.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyTrackGuide.Auth
+{
+    public class SpotifyAuthentication
+    {
+        private readonly IConfiguration _config;
+
+        public SpotifyAuthentication(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GetClientCredentialsAuthToken()
+        {
+            var spotifyClient = _config["ClientId"];
+            var spotifySecret = _config["ClientSecret"];
+
+            var webClient = new WebClient();
+
+            var postparams = new NameValueCollection();
+            postparams.Add("grant_type", "client_credentials");
+
+            var authHeader = Convert.ToBase64String(Encoding.Default.GetBytes($"{spotifyClient}:{spotifySecret}"));
+            webClient.Headers.Add(HttpRequestHeader.Authorization, "Basic " + authHeader);
+
+            var tokenResponse = webClient.UploadValues("https://accounts.spotify.com/api/token", postparams);
+
+            return Encoding.UTF8.GetString(tokenResponse);
+        }
+
+        
+    }
+}
---
+++ SpotifyTrackGuide/Controllers/SpotifyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using SpotifyTrackGuide.Auth;
+
+namespace SpotifyTrackGuide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpotifyController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private SpotifyWebAPI _api;
+
+        public SpotifyController(IConfiguration config)
+        {
+            _config = config;
+
+            SpotifyAuthentication token = new SpotifyAuthentication(_config);
+            var bearer = JsonConvert.DeserializeObject<Dictionary<string, string>>(token.GetClientCredentialsAuthToken());
+            _api = new SpotifyWebAPI
+            {
+                AccessToken = bearer["access_token"],
+                TokenType = bearer["token_type"],
+                UseAuth = true
+            };
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+            
+            PrivateProfile profile = await _api.GetPrivateProfileAsync();
+            if (!profile.HasError())
+            {
+                return new string[] { profile.DisplayName };
+            }
+
+            return new string[] { "ERROR" };
+        }
+
+ 
+    }
+}
